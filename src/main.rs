c37fe87cb37fe370a37e48e7b7348cac9819476a
Joel B <smol@toast.cyou>
2023-02-25 22:12:46 -0500

    maybe it works now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io::Write;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    println!("Listening on http://127.0.0.1:8080");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        thread::spawn(|| {
+            handle_request(stream);
+        });
+    }
+}
+
+fn handle_request(mut stream: TcpStream) {
+    let response = "HTTP/1.1 418 I'm a teapot\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
